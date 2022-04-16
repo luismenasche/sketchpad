0706a1fe698673ed59180e9b01439175037c5249
Luis Menasché <93421667+luismenasche@users.noreply.github.com>
2022-04-16 02:31:09 -0300

    First version

---
+++ README.md
@@ -0,0 +1,15 @@
+# Sketchpad
+
+This is an [exercise](https://www.theodinproject.com/lessons/foundations-etch-a-sketch) proposed in the [Odin Project](https://www.theodinproject.com/).
+
+It is a sketchpad where the user can draw by moving the mouse over it. The more the mouse goes over a space, the darker it becomes.
+
+Some functionalities that were not in the exercise description were added, like an eraser and the "Clear" button.
+
+## Technologies Employed
+
+![HTML5](https://img.shields.io/badge/HTML5-E34F26?style=for-the-badge&logo=html5&logoColor=white) ![CSS3](https://img.shields.io/badge/CSS3-1572B6?style=for-the-badge&logo=css3&logoColor=white) ![JavaScript](https://img.shields.io/badge/JavaScript-F7DF1E?style=for-the-badge&logo=javascript&logoColor=black)
+
+## Live Webpage
+
+https://luismenasche.github.io/sketchpad/
--- script.js
+++ script.js
@@ -1,17 +1,35 @@
 const header = document.getElementById("header");
+<<<<<<< HEAD
 const main = document.getElementById("main");
 const pad = document.getElementById("pad");
+=======
+>>>>>>> 215f1b4 (First version)
 const inputSqNum = document.getElementById("sqnum");
 const btBuild = document.getElementById("btbuild");
 const btPen = document.getElementById("btpen");
 const btEraser = document.getElementById("bteraser");
 const btClear = document.getElementById("btclear");
+<<<<<<< HEAD
 
 let penOn = false, eraserOn = false;
+=======
+const main = document.getElementById("main");
+const pad = document.getElementById("pad");
+const message = document.getElementById("message");
+const help = document.getElementById("help");
+const btHelp = document.getElementById("bthelp");
+
+let penOn = false, eraserOn = false, disabled = false;
+>>>>>>> 215f1b4 (First version)
 let sqNum;
 
 function buildPad() {
     let i, j, sq;
+<<<<<<< HEAD
+=======
+    if (disabled)
+        return;
+>>>>>>> 215f1b4 (First version)
     sqNum = inputSqNum.value;
     pad.replaceChildren([]);
     pad.style.gridTemplateColumns = `repeat(${sqNum},1fr)`;
@@ -22,7 +40,11 @@ function buildPad() {
             sq.setAttribute("data-light","100");
             sq.addEventListener("mouseover", ev => {
                 let light;
+<<<<<<< HEAD
                 if (!penOn && !eraserOn)
+=======
+                if ((!penOn && !eraserOn) || disabled)
+>>>>>>> 215f1b4 (First version)
                     return;
                 else if (penOn) {
                     light = Number(ev.target.getAttribute("data-light"));
@@ -41,7 +63,11 @@ function buildPad() {
 
 function sizeSquares() {
     main.style.height = `${window.innerHeight - header.offsetHeight}px`;
+<<<<<<< HEAD
     const padSize = 0.95 * Math.min(main.clientHeight, main.clientWidth);
+=======
+    const padSize = 0.98 * Math.min(main.clientHeight, main.clientWidth);
+>>>>>>> 215f1b4 (First version)
     const sqSize = Math.floor(padSize / sqNum);
     for (let sq of pad.children) {
         sq.style.width = `${sqSize}px`;
@@ -52,6 +78,11 @@ function sizeSquares() {
 }
 
 function togglePen() {
+<<<<<<< HEAD
+=======
+    if (disabled)
+        return;
+>>>>>>> 215f1b4 (First version)
     penOn = !penOn;
     btPen.classList.toggle("button--on");
     if (btPen.classList.contains("button--on"))
@@ -64,6 +95,11 @@ function togglePen() {
 }
 
 function toggleEraser() {
+<<<<<<< HEAD
+=======
+    if (disabled)
+        return;
+>>>>>>> 215f1b4 (First version)
     eraserOn = !eraserOn;
     btEraser.classList.toggle("button--on");
     if (btEraser.classList.contains("button--on"))
@@ -75,18 +111,37 @@ function toggleEraser() {
     btPen.textContent = "Pen off";
 }
 
+<<<<<<< HEAD
+=======
+function turnOff() {
+    penOn = false;
+    btPen.textContent = "Pen off";
+    btPen.classList.remove("button--on");
+    eraserOn = false;
+    btEraser.textContent = "Eraser off";
+    btEraser.classList.remove("button--on");
+}
+
+>>>>>>> 215f1b4 (First version)
 inputSqNum.value = 16;
 buildPad();
 sizeSquares();
 
 window.addEventListener("resize", sizeSquares);
+<<<<<<< HEAD
 pad.addEventListener("click", togglePen);
 btPen.addEventListener("click", togglePen);
 pad.addEventListener("contextmenu", ev => {
+=======
+main.addEventListener("click", togglePen);
+btPen.addEventListener("click", togglePen);
+main.addEventListener("contextmenu", ev => {
+>>>>>>> 215f1b4 (First version)
     ev.preventDefault();
     toggleEraser();
 });
 btEraser.addEventListener("click", toggleEraser);
+<<<<<<< HEAD
 pad.addEventListener("mouseleave", () => {
     penOn = false;
     btPen.classList.remove("button--on");
@@ -103,6 +158,26 @@ inputSqNum.addEventListener("input", () => {
     else {
         btBuild.disabled = false;
         btBuild.textContent = "Build";
+=======
+inputSqNum.addEventListener("input", () => {
+    message.textContent = "";
+    if (!inputSqNum.checkValidity()) {
+        btBuild.disabled = true;
+        message.textContent = inputSqNum.validationMessage;
+        turnOff();
+        btPen.disabled = true;
+        btEraser.disabled = true;
+        disabled = true;
+    }
+    else {
+        btPen.disabled = false;
+        btEraser.disabled = false;
+        disabled = false;
+        if (inputSqNum.value == "")
+            btBuild.disabled = true;
+        else
+            btBuild.disabled = false;
+>>>>>>> 215f1b4 (First version)
     }
 });
 btBuild.addEventListener("click", buildPad);
@@ -111,4 +186,22 @@ btClear.addEventListener("click", () => {
         sq.setAttribute("data-light", 100);
         sq.style.backgroundColor = "white";
     }
+<<<<<<< HEAD
+=======
+    turnOff();
+});
+btHelp.addEventListener("click", ev => {
+    ev.stopPropagation();
+    btHelp.classList.add("button--on");
+    disabled = true;
+    inputSqNum.disabled = true;
+    help.style.display = "block";
+});
+help.addEventListener("click", ev => {
+    ev.stopPropagation();
+    btHelp.classList.remove("button--on");
+    disabled = false;
+    inputSqNum.disabled = false;
+    help.style.display = "none";
+>>>>>>> 215f1b4 (First version)
 });
